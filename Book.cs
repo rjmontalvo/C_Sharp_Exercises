6c3b31577f5883e20f38cd8180ef983b90e97ded
Raven Montalvo <raven.montalvo@outlook.com>
2020-03-11 22:22:36 -0700

    Add Book class. Instantiate in Program.cs

---
+++ Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        List<double> grades;
+        string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            if (grade >= 0 || grade <= 100)
+            {
+                grades.Add(grade);
+            }
+        }
+
+        public double HighestGrade()
+        {
+            var highGrade = double.MinValue;
+            foreach (var grade in grades)
+            {
+                highGrade = Math.Max(highGrade, grade);
+            }
+            return highGrade;
+        }
+
+        public double LowestGrade()
+        {
+            var lowGrade = double.MaxValue;
+            foreach (var grade in grades)
+            {
+                lowGrade = Math.Min(lowGrade, grade);
+            }
+            return lowGrade;
+        }
+
+        public double AvgGrade()
+        {
+            var total = 0.0;
+            foreach (var grade in grades)
+            {
+                total += grade;
+            }
+            var result = total / grades.Count;
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GradeBook;
 
 namespace CSharp_Coding_Exercises
 {
@@ -10,6 +11,13 @@ namespace CSharp_Coding_Exercises
     {
         static void Main(string[] args)
         {
+            Book book = new Book("My Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            Console.WriteLine(book.AvgGrade());
+            Console.WriteLine(book.LowestGrade());
+            Console.WriteLine(book.HighestGrade());
         }
     }
 }
