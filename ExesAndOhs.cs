97c438889ce6cf2e91330302e38ae46054f8ef02
rjmontalvo <rjmontalvo@users.noreply.github.com>
2020-03-20 19:15:42 -0700

    Completed from CodeWars

---
+++ ExesAndOhs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Coding_Exercises
+{
+    class ExesAndOhs
+    {
+        /*Check to see if a string has the same amount of 'x's and 'o's. 
+         * The method must return a boolean and be case insensitive. 
+         * The string can contain any char.*/
+        public static bool XO(string input)
+        {
+            return 
+                input.ToLower().Where(i => i.Equals('x')).Count() == 
+                input.ToLower().Where(i => i.Equals('o')).Count(); 
+ 
+        }
+    }
+}
---
+++ TwoToOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Coding_Exercises
+{
+    class TwoToOne
+    {
+        /*Take 2 strings s1 and s2 including only letters from ato z. 
+         * Return a new sorted string, the longest possible, containing distinct letters, each taken only once - coming from s1 or s2.*/
+        public static string Longest(string s1, string s2)
+        {
+            //Create a character array of distinct letters from each string
+            char[] distinct = s1.Distinct().Union(s2.Distinct()).ToArray<char>();
+            
+            //Sort the array
+            Array.Sort(distinct);
+            
+            //Return all the characters in the form of a string
+            return String.Concat(distinct);
+        }
+
+    }
+}
